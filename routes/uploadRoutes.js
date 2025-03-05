8ffe211f02c43e4c34693c0a98e89696cca3bd01
Bhakti-Dhanani <bhagvatiben.wappnet@gmail.com>
2025-03-05 09:47:33 +0530

    aadd reccommendation changes

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const multer = require("multer");
 const {uploadResume, getResume, getAllResumes,deleteResume}  = require("../controllers/uploadController");
 const {authMiddleware} = require("../middleware/authMiddleware");
-const RecommendedJob = require("../models/RecommendedJob");
 const Resume = require("../models/resumes");
 const router = express.Router();
 
@@ -15,19 +14,4 @@ router.get("/resume",authMiddleware, getResume);
 router.delete("/delete",authMiddleware, deleteResume);
 router.get("/resumes", getAllResumes);
 
-
-
-// Get recommended jobs for a user
-router.get("/user/:userId/recommended-jobs", async (req, res) => {
-  try {
-    const { userId } = req.params;
-
-    const recommendations = await RecommendedJob.find({ user_id: userId }).populate("job_id");
-
-    res.json({ success: true, recommendations });
-  } catch (error) {
-    console.error("Error fetching recommended jobs:", error);
-    res.status(500).json({ success: false, error: error.message });
-  }
-});
 module.exports = router;
